febe5b733b803f8080eab3033046b996d0ab6303
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-02-06 18:47:40 -0400

    Reset migrations and update EF Core to 9.0.1

--- Data/Migrations/20250206224700_inicial.Designer.cs
+++ Data/Migrations/20250206224700_inicial.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EyePocket.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250206191844_inicial")]
+    [Migration("20250206224700_inicial")]
     partial class inicial
     {
         /// <inheritdoc />
@@ -20,7 +20,7 @@ namespace EyePocket.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "9.0.0")
+                .HasAnnotation("ProductVersion", "9.0.1")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
