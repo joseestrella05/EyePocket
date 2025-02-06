b9d85ad52ae1e47a5214a6b94a063fec8531a7b8
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-02-06 15:13:33 -0400

    Add DbSets and seeding to ApplicationDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,22 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Shared.Models;
 
-namespace EyePocket.Data
+namespace EyePocket.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    public DbSet<Clientes> Clientes { get; set; }
+
+    public DbSet<Estados> Estados { get; set; }
+
+    public void ConfigureGeneralModel(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Estados>().HasData(
+             new Estados { EstadoId = 1, Nombre = "Activo" },
+             new Estados { EstadoId = 2, Nombre = "Inactivo" }
+        );
     }
+
+
 }
