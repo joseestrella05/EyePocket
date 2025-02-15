c6f3f6cae3d68b1618c670b0ed53ceba34043725
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2025-02-14 22:12:18 -0400

    Se creo el modelo de metodo de pago

---
+++ Models/MetodoPago.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EyePocket.Models;
+
+public class MetodoPago
+{
+    [Key]
+    public int MetodoPagoId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    [RegularExpression(@"^[\p{L}\s,]+$", ErrorMessage = "Solo se permiten letras, espacios, comas y acentos.")]
+    public string? Descripcion { get; set; }
+}
