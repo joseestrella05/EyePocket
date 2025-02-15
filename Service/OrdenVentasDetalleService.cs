30354f25dd7e068a8fb0228ccc40c32bff0ff2e7
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2025-02-14 22:53:51 -0400

    Se crearon los servicios de orden ventas detalles

---
+++ Service/OrdenVentasDetalleService.cs
@@ -0,0 +1,20 @@
+﻿using EyePocket.Data;
+using EyePocket.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace EyePocket.Service
+{
+    public class OrdenVentasDetalleService(IDbContextFactory<ApplicationDbContext> DbFactory)
+    {
+        public async Task<List<OrdenVentaDetalle>> Listar(Expression<Func<OrdenVentaDetalle, bool>> criterio)
+        {
+            await using var _contexto = await DbFactory.CreateDbContextAsync();
+
+            return await _contexto.OrdenVentasDetalle
+                .AsNoTracking()
+                .Where(criterio)
+                .ToListAsync();
+        }
+    }
+}
