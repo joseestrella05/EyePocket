c694f64d9ef6929fdbc090f0a4b34ef82155ba4d
Yohualkis <Yohualkis99@gmail.com>
2025-02-21 13:02:52 -0400

    Se creo la clase PagosCXC

---
+++ Models/PagosCXC.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EyePocket.Models;
+
+public class PagosCXC
+{
+	[Key]
+	public int Id { get; set; }
+
+
+	[ForeignKey("DeudaId")]
+	public int CXCId { get; set; }
+
+	public string? Descripcion { get; set; }
+
+
+	public DateTime FechaPago { get; set; } = DateTime.Now;
+
+
+	public int MetodoPagoId { get; set; }
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public double MontoPagado { get; set; }
+}
