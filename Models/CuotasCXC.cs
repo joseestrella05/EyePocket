ba972b3c599ec39b15f9018ca6133681dd5e2508
Yohualkis <Yohualkis99@gmail.com>
2025-02-26 12:05:58 -0400

    Se creo la clase cuotasCXC

---
+++ Models/CuotasCXC.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EyePocket.Models;
+
+public class CuotasCXC
+{
+	[Key]
+	public int CuotaCXCID { get; set; }
+
+
+	[ForeignKey("CXCId")]
+	public int CXCId { get; set; }
+
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public DateTime FechaVencimiento { get; set; }
+
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public double MontoCuota { get; set; }
+
+
+	public int EstadoId { get; set; }
+	public Estados? Estado { get; set; }
+}
