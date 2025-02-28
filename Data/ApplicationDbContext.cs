5b0291e150a6d5f8589d5c39d0177578a4188b14
Yohualkis <Yohualkis99@gmail.com>
2025-02-28 13:00:00 -0400

    Se agregaron los respectivos campos de entrada y validaciones al form en base a los nuevos campos de la clase CuentasXCobrar

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -7,20 +7,20 @@ namespace EyePocket.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
-    public DbSet<Clientes> Clientes { get; set; }
+	public DbSet<Clientes> Clientes { get; set; }
 
-    public DbSet<Estados> Estados { get; set; }
+	public DbSet<Estados> Estados { get; set; }
 
-    public DbSet<CuentasXCobrar> CuentasXCobrar { get; set; }
+	public DbSet<CuentasXCobrar> CuentasXCobrar { get; set; }
 
-    public DbSet<Productos> Productos { get; set; }
+	public DbSet<Productos> Productos { get; set; }
 
-    public DbSet<OrdenVenta> OrdenVenta { get; set; }
-    public DbSet<Tickets> Tickets { get; set; }
+	public DbSet<OrdenVenta> OrdenVenta { get; set; }
+	public DbSet<Tickets> Tickets { get; set; }
 
-    public DbSet<MetodosPago> MetodosPago { get; set; }
-    public DbSet<PagosCXC> PagosCXC { get; set; }
-    public DbSet<CuotasCXC> CuotasCXC { get; set; }
+	public DbSet<MetodosPago> MetodosPago { get; set; }
+	public DbSet<PagosCXC> PagosCXC { get; set; }
+	public DbSet<CuotasCXC> CuotasCXC { get; set; }
 
 
 	protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -34,6 +34,12 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 			new MetodosPago { MetodoPagoId = 1, Descripcion = "Tarjeta" },
 			new MetodosPago { MetodoPagoId = 2, Descripcion = "Efectivo" },
 			new MetodosPago { MetodoPagoId = 3, Descripcion = "Cheque" }
+		);
+
+		modelBuilder.Entity<Estados>().HasData(
+			new Estados { EstadoId = 1, Nombre = "Pendiente" },
+			new Estados { EstadoId = 2, Nombre = "Pagado" },
+			new Estados { EstadoId = 3, Nombre = "Vencido" }
 		);
 	}
 }
