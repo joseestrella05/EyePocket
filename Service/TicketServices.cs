8c0ab76bf33585462b4ee3cac3e98eb295d06281
erick <erickestradarosario@gmail.com>
2025-02-27 20:37:20 -0400

    Corregi el metodo existe cedula

--- Service/TicketServices.cs
+++ Service/TicketServices.cs
@@ -23,7 +23,7 @@ public class TicketServices (IDbContextFactory<ApplicationDbContext> DbFactory)
     public async Task<bool> ExisteCedula(int ClienteId, string cedula)
     {
         await using var contexto = await DbFactory.CreateDbContextAsync();
-        return await contexto.Clientes.AnyAsync(c => c.Cedula == cedula && c.ClienteId != ClienteId);
+        return await contexto.Tickets.AnyAsync(t => t.Cliente.Cedula == cedula && t.Cliente.ClienteId != ClienteId);
     }
 
     public async Task<bool> Existe(int id)
