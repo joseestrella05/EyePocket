f4ec821954d8fd3e4ce70b44549f298386c1adec
Yohualkis <Yohualkis99@gmail.com>
2025-03-03 05:40:49 -0400

    Se generizaron los mensajes de error

--- Models/Clientes.cs
+++ Models/Clientes.cs
@@ -13,28 +13,28 @@ public class Clientes
     [Key]
     public int ClienteId { get; set; }
 
-    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "No se permiten Numero")]
-    [Required(ErrorMessage = "El Nombres obligatorio")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Este campo no admite números ni caractereres especiales")]
+    [Required(ErrorMessage = "Este campo es obligatorio")]
     public string? Nombre { get; set; }
 
     public string? Apellido { get; set; }
 
-    [Required(ErrorMessage = "Por favor agrega una cedula o Rnc ")]
-    [RegularExpression(@"^[A-Z0-9\d{2}]+$", ErrorMessage = "Solo se permiten Numero")]
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	[RegularExpression(@"^[A-Z0-9\d{2}]+$", ErrorMessage = "Solo se permiten Numero")]
     public string? Cedula { get; set; }
 
-    [Required(ErrorMessage = "La dirrecion es obligatoria")]
-    public string? Direccion { get; set; }
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public string? Direccion { get; set; }
 
-    [Required(ErrorMessage = "El Telefono es obligatorio")]
-    [RegularExpression(@"^[A-Z0-9\d{2}]+$", ErrorMessage = "Solo se permiten Numero")]
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	[RegularExpression(@"^[A-Z0-9\d{2}]+$", ErrorMessage = "Solo se permiten Numero")]
     public string? Telefono { get; set; }
 
-    [Required(ErrorMessage = "La fecha es obligatoria")]
-    public DateTime FechaIngreso { get; set; }
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public DateTime FechaIngreso { get; set; } = DateTime.Now;
 
-    [Required(ErrorMessage = "El Email es obligatorio")]
-    public string? Email { get; set; }
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public string? Email { get; set; }
 
     public bool Activo { get; set; }
 
