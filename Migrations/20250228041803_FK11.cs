5300de8d79979f9bdc04566cc34b68e4f4227fe3
Yohualkis <Yohualkis99@gmail.com>
2025-03-04 12:48:44 -0400

    Se migraron los datos

---
+++ Migrations/20250228041803_FK11.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EyePocket.Migrations
+{
+    /// <inheritdoc />
+    public partial class FK11 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
