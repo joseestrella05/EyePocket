7248991d341557f4c7db069e34b1b27b09899b85
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2025-03-12 21:31:20 -0400

    Migration update

---
+++ Migrations/20250313002844_NumeroFacturaToOrdenVentas.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EyePocket.Migrations
+{
+    /// <inheritdoc />
+    public partial class NumeroFacturaToOrdenVentas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Codigo",
+                table: "OrdenVentas");
+
+            migrationBuilder.RenameColumn(
+                name: "NCF",
+                table: "OrdenVentas",
+                newName: "NumeroFactura");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NumeroFactura",
+                table: "OrdenVentas",
+                newName: "NCF");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Codigo",
+                table: "OrdenVentas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
