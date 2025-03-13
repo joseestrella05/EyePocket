055e2c18427ad578a08c8761be5d324c9d29733d
fahh7 <fahh7@FREIMYHP>
2025-03-13 19:40:29 -0400

    Quitando el Datatime.Now de Fecha ingreso(Daba error al crear la migracion).

--- Models/Productos.cs
+++ Models/Productos.cs
@@ -9,10 +9,10 @@ namespace EyePocket.Models
         public int ProductoId { get; set; }
 
         [Required(ErrorMessage = "El código es obligatorio.")]
-        public string Codigo { get; set; }
+        public string? Codigo { get; set; }
 
         [Required(ErrorMessage = "El nombre del producto es obligatorio.")]
-        public string Nombre { get; set; }
+        public string? Nombre { get; set; }
 
         [Required(ErrorMessage = "El precio es obligatorio.")]
         [Range(0, double.MaxValue, ErrorMessage = "El precio debe ser mayor o igual a 0.")]
@@ -23,16 +23,16 @@ namespace EyePocket.Models
         public decimal Costo { get; set; }
 
         [Required(ErrorMessage = "Debe seleccionar una categoría.")]
-        public string Categoria { get; set; }
+        public string? Categoria { get; set; }
 
-        public string Proveedor { get; set; }
+        public string? Proveedor { get; set; }
         [Required(ErrorMessage ="Describa el producto")]
-        public string Descripcion { get; set; }
+        public string? Descripcion { get; set; }
 
         [Range(0, 100, ErrorMessage = "El descuento debe estar entre 0 y 100.")]
         public decimal Descuento { get; set; }
 
         [Required(ErrorMessage = "La fecha de ingreso es obligatoria.")]
-        public DateTime FechaIngreso { get; set; } = DateTime.Now;
+        public DateTime FechaIngreso { get; set; }
     }
 }
