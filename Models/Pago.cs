5408004016a31ef2eea10cccc22509e4ee4e01ec
fahh7 <fahh7@FREIMYHP>
2025-03-21 08:37:37 -0400

    Creando modelo pago

---
+++ Models/Pago.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EyePocket.Models
+{
+    public class Pago
+    {
+
+        [Key]
+
+        public int PagoId { get; set; }
+
+        public int MetodoPagoId { get; set; }
+
+        [ForeignKey("MetodoPagoId")]
+        public virtual MetodoPago MetodoPago { get; set; } = null!;
+
+        
+        public int CuentaPorPagarId { get; set; }
+
+        [ForeignKey("CuentaPorPagarId")]
+        public virtual CXP CuentaPorPagar { get; set; } = null!;
+
+        [Required(ErrorMessage = "Favor de introducir una fecha")]
+        public DateTime FechaPago { get; set; }
+
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public double monto { get; set; }
+
+    }
+}
