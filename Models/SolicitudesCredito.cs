984676e1a96d25f985a32fe27368cb476a062335
Yohualkis <Yohualkis99@gmail.com>
2025-03-23 01:06:43 -0400

    Se creo el nuevo modelo

---
+++ Models/SolicitudesCredito.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EyePocket.Models;
+
+public class SolicitudesCredito
+{
+	[Key]
+	public int SolicitudCreditoId { get; set; }
+
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public int ClienteId { get; set; }
+	public Clientes? Cliente { get; set; }
+
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public double MontoSolicitado { get; set; }
+
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public double SueldoFijo{ get; set; }
+
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public string Garantia { get; set; }
+
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public string DescripcionGarantia { get; set; }
+
+
+	public bool EstatusDataCredito { get; set; } = true;
+}
