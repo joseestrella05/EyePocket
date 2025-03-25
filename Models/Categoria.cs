9e6d6600edbd046dad1e26ac3a0b11d5dffd51cd
frailyng <frailindes@gmail.com>
2025-03-24 21:47:00 -0400

    Modelo Cateogoria Creado

---
+++ Models/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EyePocket.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int CategoriaId { get; set; }
+
+        [Required(ErrorMessage = "Debe seleccionar el nombre de la categoria.")]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+    }
+
+
+}
