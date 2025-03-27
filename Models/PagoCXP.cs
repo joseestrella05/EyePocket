565d7d79c7ae7e0813c71d7d34e682ef391aac99
fahh7 <fahh7@FREIMYHP>
2025-03-26 21:20:17 -0400

    Creacion de modelos csp, pago, estado

---
+++ Models/PagoCXP.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EyePocket.Models
+{
+    public class PagoCXP
+    {
+
+        [Key]
+
+        public int PagoId { get; set; }
+
+        public int MetodoPagoId { get; set; }
+
+        [ForeignKey("MetodoPagoId")]
+        public virtual MetodosPago MetodoPago { get; set; } = null!;
+
+        public int CuentaPorPagarId { get; set; }
+
+        [ForeignKey("CuentaPorPagarId")]
+        public virtual CXP CuentaPorPagar { get; set; } = null!;
+
+        [Required(ErrorMessage = "Favor de introducir una fecha")]
+        public DateTime FechaPago { get; set; }
+
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public double monto { get; set; }
+
+
+    }
+}
