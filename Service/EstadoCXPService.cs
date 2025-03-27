d703cd1da9f73c2b843b743fdd5e3f5c97a6c744
fahh7 <fahh7@FREIMYHP>
2025-03-26 21:22:01 -0400

    Services

---
+++ Service/EstadoCXPService.cs
@@ -0,0 +1,30 @@
+﻿using EyePocket.Data;
+using EyePocket.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace EyePocket.Service
+{
+    public class EstadoCXPService(IDbContextFactory<ApplicationDbContext> DbFactory)
+    {
+
+        public async Task<List<EstadoCXP>> Listar(Expression<Func<EstadoCXP, bool>> criterio)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.EstadoCXP.AsNoTracking()
+                .Where(criterio)
+                .ToListAsync();
+        }
+
+
+        public async Task<EstadoCXP?> Buscar(int id)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.EstadoCXP.AsNoTracking().
+                FirstOrDefaultAsync(p => p.EstadoCXPId == id);
+        }
+
+
+
+    }
+}
