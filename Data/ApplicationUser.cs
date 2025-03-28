7148796942a3be5c68b2b1ddcf6f54f42a52fb6b
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-03-28 01:35:03 -0400

    Update seeding and properties in ApplicationDbContext
    - Removed configuration for IdentityUserLogin<string> and seeding of MetodosPago in ApplicationDbContext.
    - Added seeding for IdentityRole with roles: Ventas, CuentasXCobrar, CuentasXPagar, Inventario, and ServicioAlCliente.
    - Updated Estados seeding to include new states: Cancelado, Aprobado, and Denegado.
    - Added FechaIngreso and FechaNacimiento properties to ApplicationUser, both initialized to the current date and time.

--- Data/ApplicationUser.cs
+++ Data/ApplicationUser.cs
@@ -5,6 +5,8 @@ namespace EyePocket.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public DateTime FechaIngreso { get; set; } = DateTime.Now;
+        public DateTime FechaNacimiento { get; set; } = DateTime.Now;
     }
 
 }
