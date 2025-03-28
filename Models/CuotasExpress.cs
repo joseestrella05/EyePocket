df412820a220a003443af005beabe0ff72d92c43
Yohualkis <Yohualkis99@gmail.com>
2025-03-28 07:56:05 -0400

    Add CuotasExpress class to EyePocket.Models namespace
    Introduced a new namespace `EyePocket.Models` and added a new class `CuotasExpress` within it. The `CuotasExpress` class includes five properties: `NumeroCuota` (int), `Monto` (double), `Interes` (double), `SaldoFinal` (double), and `Periodos` (int), which represent various financial attributes.

---
+++ Models/CuotasExpress.cs
@@ -0,0 +1,10 @@
+﻿namespace EyePocket.Models;
+
+public class CuotasExpress
+{
+	public int NumeroCuota { get; set; }
+	public double Monto { get; set; }
+	public double Interes { get; set; }
+	public double SaldoFinal { get; set; }
+	public int Periodos { get; set; }
+}
