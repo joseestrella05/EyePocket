13598c3eea9392ebaa0fe179880ef192394a6fc3
znecu <alma_maria@ucne.edu.do>
2025-04-09 22:30:57 -0400

    Se creó el service de cierre de caja

---
+++ Service/CierreCajaService.cs
@@ -0,0 +1,74 @@
+﻿using EyePocket.Data;
+using EyePocket.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace EyePocket.Service;
+
+public class CierreCajaService(IDbContextFactory<ApplicationDbContext> DbFactory)
+{
+    public async Task<bool> Guardar(CierreCaja cierreCaja)
+    {
+        await using var _contexto = await DbFactory.CreateDbContextAsync();
+
+        if (!await Existe(cierreCaja.CierreId))
+        {
+            return await Insertar(cierreCaja);
+        }
+        else
+        {
+            return await Modificar(cierreCaja);
+        }
+    }
+    private async Task<bool> Existe(int CierreCajaId)
+    {
+        await using var _contexto = await DbFactory.CreateDbContextAsync();
+
+        return await _contexto.CierreCaja
+            .AnyAsync(c => c.CierreId == CierreCajaId);
+    }
+    private async Task<bool> Insertar(CierreCaja cierreCaja)
+    {
+        await using var _contexto = await DbFactory.CreateDbContextAsync();
+
+        _contexto.CierreCaja.Add(cierreCaja);
+        return await _contexto.SaveChangesAsync() > 0;
+    }
+
+    private async Task<bool> Modificar(CierreCaja cierreCaja)
+    {
+        await using var _contexto = await DbFactory.CreateDbContextAsync();
+
+        _contexto.Update(cierreCaja);
+        return await _contexto.SaveChangesAsync() > 0;
+
+    }
+
+    public async Task<bool> Eliminar(CierreCaja cierreCaja)
+    {
+        await using var _contexto = await DbFactory.CreateDbContextAsync();
+
+        return await _contexto.CierreCaja
+            .AsNoTracking()
+            .Where(c => c.CierreId == cierreCaja.CierreId)
+            .ExecuteDeleteAsync() > 0;
+    }
+
+    public async Task<CierreCaja?> Buscar(int cierreCajaId)
+    {
+        await using var _contexto = await DbFactory.CreateDbContextAsync();
+
+        return await _contexto.CierreCaja
+            .AsNoTracking()
+            .FirstOrDefaultAsync(c => c.CierreId == cierreCajaId);
+    }
+    public async Task<List<CierreCaja>> Listar(Expression<Func<CierreCaja, bool>> criterio)
+    {
+        await using var _contexto = await DbFactory.CreateDbContextAsync();
+
+        return await _contexto.CierreCaja
+            .AsNoTracking()
+            .Where(criterio)
+            .ToListAsync();
+    }
+}
