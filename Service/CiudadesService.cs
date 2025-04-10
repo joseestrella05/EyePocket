ab05e4bf9a0a6c397eb9a838a29710dfc1ebb745
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-04-11 00:17:06 +0200

    Arreglado errores

--- Service/CiudadesService.cs
+++ Service/CiudadesService.cs
@@ -43,7 +43,7 @@ public class CiudadesService(IDbContextFactory<ApplicationDbContext> DbFactory)
     public async Task<Ciudades?> Buscar(int CiudadId)
     {
         await using var Contexto = await DbFactory.CreateDbContextAsync();
-        return await Contexto.Ciudades.Include(c => c.Cliente).FirstOrDefaultAsync(c => c.CiudadId == CiudadId);
+        return await Contexto.Ciudades.FirstOrDefaultAsync(c => c.CiudadId == CiudadId);
     }
 
     public async Task<bool> Eliminar(int ciudadId)
