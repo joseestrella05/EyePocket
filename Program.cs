0a416d6882dc2184e72b8ff08ae2519a8b24e6c5
Yohualkis <Yohualkis99@gmail.com>
2025-04-13 13:55:13 -0400

    Se inyecto el service para el QuestPDF

--- Program.cs
+++ Program.cs
@@ -7,11 +7,20 @@ using EyePocket.Service;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using MudBlazor.Services;
 using OfficeOpenXml;
+using QuestPDF.Infrastructure;
 
 var modelbuilder = WebApplication.CreateBuilder(args);
 
 ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+QuestPDF.Settings.License = LicenseType.Community;
+
+modelbuilder.Services.AddLogging(config =>
+{
+	config.AddConsole();
+	config.SetMinimumLevel(LogLevel.Debug); // Esto captura todos los logs desde debug hacia arriba
+});
 
 // Add services to the container.
 modelbuilder.Services.AddRazorComponents()
@@ -62,6 +71,9 @@ modelbuilder.Services.AddScoped<OrdenVentasDetalleService>();
 modelbuilder.Services.AddScoped<SolicitudesCreditoService>();
 modelbuilder.Services.AddScoped<CategoriaService>();
 modelbuilder.Services.AddScoped<DistribucionInventarioService>();
+
+// MudBlazor
+modelbuilder.Services.AddMudServices();
 
 //notificacion
 modelbuilder.Services.AddBlazoredToast();
